e033d9be9acd33a0dd5a10b125636559a39e5b6e
trdev7 <trdev7@africa.int.zenithbank.com>
2020-01-02 08:36:28 +0100

    test call to another express instance

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const authenticate = require('./controllers/authenticate');
 const logger = require('./middleware/Loggger');
 const Response = require('./models/Response');
 const apiAdvise = require('./middleware/ApiAdvise');
+const fetch = require('node-fetch');
 
 //middlewares init
 app.use([logger, apiAdvise]);
@@ -18,6 +19,21 @@ app.use(express.urlencoded({ extended: false }))
 app.use('/authenticate', authenticate);
 
 app.get('/', (req, res )=> res.send('hello world'));
+
+app.get('/callSecondServer', (req, res) => {
+    // fetch('http://localhost:5000/').then(result => {
+    //     result.json().then(r => {
+    //         console.log(r);
+    //         res.send(r);
+    //     });
+    // });
+    (async () => {
+        const response = await fetch('http://localhost:5000/');
+        const body = await response.json();
+
+        res.send(body);
+    })();
+});
 
 
 //Set static folder
@@ -34,7 +50,7 @@ app.use((err, req, res, next)=>{
     console.log(err.stack)
     res.status(500);
     res.json(Response.error(`500 Internal Server error: ${err.message}`));
-})
+});
 
 let PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Application Started on port: ${PORT}`));
--- middleware/ApiAdvise.js
+++ middleware/ApiAdvise.js
@@ -2,15 +2,16 @@ const response = require('../models/Response');
 
 //init middleware
 const ApiAdvise = (req, res, next) => {
-//getting the URL and date
-var oldSend = res.send;
+    //getting the URL and date
+    var oldSend = res.send;
 
-res.send = function() {
-    console.log("Api addvise");
-    arguments[0] = JSON.stringify(new response(arguments[0]));
-    oldSend.apply(res, arguments);
-}
-next();
+    res.set("Content-Type", "appliation/json");
+    res.send = function() {
+        console.log("Api addvise");
+        arguments[0] = JSON.stringify(new response(arguments[0]));
+        oldSend.apply(res, arguments);
+    }
+    next();
 };
 
 module.exports = ApiAdvise;
